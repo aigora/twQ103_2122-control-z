21b07c94864685261c5b92c940ef12c02d8da896
Franciscojavier03 <100997552+Franciscojavier03@users.noreply.github.com>
2022-04-03 18:46:22 +0200

    flexeando con el menu

--- programa.c
+++ programa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define TAM_MAX 4
+#include<stdlib.h>
 
 struct Dusuario {
     char nombre[50];
@@ -9,23 +10,32 @@ struct Dusuario {
 };
 int main () {
 	struct Dusuario usuarios[TAM_MAX];
-
-    int tam_users,i=0,edad;
-
-    printf("Al fin te hemos encontrado!\n");
-    printf("\n");
-    printf("Bueno dejame contarte, antes de que perdieras la memoria trabajabas para la empresa de modificacion genetica 'Control Z'");
-	printf("encargada de crear bacterias y virus para erradicar todas las enfermedades del mundo.\n");
-	printf("\n");
-    printf("Y asi fue, hoy en dia no hay ni malaria ni COVID19  entre otras enfermedades, pero...\n");
-    printf("una alteracion genetica en uno de esos nuevos virus permitio que llegara a los humanos a los cuales transforma en... \n");
-    printf("ZOMBIES!!!!\n");
-    printf("\n");
-    printf("Hemos denominado al virus en cuestion 'VirusZ'\n");
-    printf("Tu mision es encontrar la cura a VirusZ antes de que el 100%% de la poblacion se convierta en zombie\n");
-    printf("\n");
-    printf("Ya que sabes lo que tienes que hacer. Hablame de ti: \n");
-
+	char color;
+    int tam_users,i=0,edad,menu=0;
+    system("COLOR 04");
+    while (menu!=3)	{
+    	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    	printf("\n\t\t\t\tcontrol z,\t control z,\t CONTROL Z!!!!!!!!!\n");
+    	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    	printf("\n %c INICIAR PARTIDA(1)\n\n %c CONFIGURACION(2)\n\n %c SALIR(3)\n",175,175,175);
+    	printf("\n\n%c Indique que desea hacer con el numero correspondiente:\t",175);
+    	scanf("%d",&menu);
+    	if(menu==1)	{
+    			system("cls");
+    			printf("Al fin te hemos encontrado!\n");
+    			printf("\n");
+   				printf("Bueno dejame contarte, antes de que perdieras la memoria trabajabas para la empresa de modificacion genetica 'Control Z'");
+				printf("encargada de crear bacterias y virus para erradicar todas las enfermedades del mundo.\n");
+				printf("\n");
+    			printf("Y asi fue, hoy en dia no hay ni malaria ni COVID19  entre otras enfermedades, pero...\n");
+    			printf("una alteracion genetica en uno de esos nuevos virus permitio que llegara a los humanos a los cuales transforma en... \n");
+    			printf("ZOMBIES!!!!\n");
+    			printf("\n");
+   	 			printf("Hemos denominado al virus en cuestion 'VirusZ'\n");
+    			printf("Tu mision es encontrar la cura a VirusZ antes de que el 100%% de la poblacion se convierta en zombie\n");
+    			printf("\n");
+    			printf("Ya que sabes lo que tienes que hacer. Hablame de ti: \n");
+    			//tabular
     do{
         printf("Escriba su edad (Este juego es para mayores de 12 anhos) \n");
         scanf("%d",&edad);
@@ -47,7 +57,33 @@ int main () {
         printf("Escriba su username \n");
         gets(usuarios[i].username);
         i++;
-    }
-
-	return 0;
-}
+	}
+	//solo para cuando solo haya menu
+		printf("\nJugar de nuevo? SI(1) NO(3)\n");
+			scanf("%d",&menu);
+			system("cls");
+	//tabular
+		}else if (menu==2)	{
+			system("cls");
+			fflush(stdin);
+		
+			printf("CONFIGURACION DE COLORES DE LAS LETRAS DE LA INTERFAZ\n");
+		
+			printf("Verde(V),Rojo(R),Amarillo(A),Blanco(B):\t");
+		
+			scanf("%c",&color);
+		
+			if (color=='V' || color=='v')	{
+				system("COLOR 02");
+				
+			}else if (color=='r' || color=='R')	{
+				system("COLOR 04");
+			}else if (color=='a' || color=='A')	{
+				system("COLOR 06");
+			}else if (color=='b' || color=='B')	{
+				system("COLOR 07");
+			}
+			system("cls");
+		}
+	}
+} 
