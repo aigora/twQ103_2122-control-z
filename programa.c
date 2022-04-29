06097efab20de650aed897d4aa7510235c227b0b
carloscasete <carlos.gomez.casete@alumnos.upm.es>
2022-04-29 17:29:09 +0200

    junte todo

--- programa.c
+++ programa.c
@@ -1,18 +1,84 @@
 #include <stdio.h>
 #define TAM_MAX 4
 #include<stdlib.h>
+#include <string.h>
+#define TAM_MAX_GUARDADOS 300
 
 struct Dusuario {
     char nombre[50];
-    char apellidos[100];
+    char apellido[100];
     char email[50];
     char username[50];
+    int puntuacion;
 };
+
+//funciones de las preguntas
+
+char preguntaA() {
+	
+	char opcionA;
+	scanf("%c", &opcionA);
+	if (opcionA =='A'||opcionA =='a') {
+		
+		
+		printf("CORRECTO!!!\n");
+	}
+	else {
+		printf("pfff un poco mal eh\n");
+	}
+	return opcionA;
+}
+	
+char preguntaB() {
+	
+	char opcionB;
+		scanf("%c", &opcionB);
+	if (opcionB =='B'||opcionB =='b') {
+		
+		
+		printf("CORRECTO!!!\n");
+	}
+	else {
+		printf("pfff un poco mal eh\n");
+	}
+return opcionB;
+}
+
+char preguntaC() {
+	
+	char opcionC;
+	
+	scanf("%c", &opcionC);
+	if (opcionC =='C'||opcionC =='c') {
+		
+		
+		printf("CORRECTO!!!\n");
+	}
+	else {
+		printf("pfff un poco mal eh\n");
+		
+	}
+	return opcionC;
 int main () {
+	FILE*pfile;
 	struct Dusuario usuarios[TAM_MAX];
+	struct Dusuario usuariosg[TAM_MAX_GUARDADOS];
 	char color;
     int tam_users,i=0,edad,menu=0,respuesta;
-    system("COLOR 04");
+    int j=0,orden,ntotalg;
+
+    pfile=fopen("ficheru.txt","r");
+        if(pfile==NULL){
+            printf("error");
+            return 0;
+        }else{
+            while(fscanf(pfile,"%s %s %s %s %d",usuariosg[i].nombre,usuariosg[i].apellido,usuariosg[i].email,usuariosg[i].username,&usuariosg[i].puntuacion)!=EOF){
+                    i++;
+                    ntotalg++;
+            }
+        }
+        fclose(pfile);
+	system("COLOR 04");
     while (menu!=3)	{
     	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
     	printf("\n\t\t\t\tcontrol z,\t control z,\t CONTROL Z!!!!!!!!!\n");
@@ -46,49 +112,59 @@ int main () {
         scanf("%d",&tam_users);
     }while(tam_users>4);
 
+	//ficheros
+    i=0;
     while(tam_users>i){
-        fflush(stdin);
-        printf("Escriba su nombre cientifico %d \n",i+1);
-        gets(usuarios[i].nombre);
-        printf("Escriba sus apellidos \n");
-        gets(usuarios[i].apellidos);
-        printf("Escriba su email \n");
-        gets(usuarios[i].email);
-        printf("Escriba su username \n");
-        gets(usuarios[i].username);
-        i++;
-	}
-	for(i=0;tam_users>i;i++){
-        printf("Bienvenido cientifico %s \n",usuarios[i].username);
-	}
+            printf("Si es un nuevo usuario pulse 1 si es un usuario antiguo pulse 2 \n");
+            scanf("%d",&respuesta);
 
-	printf("Pulse 1 si desea hacer el tutorial, pulse cualquier otra tecla si no lo necesita \n");
-	scanf("%d",&respuesta);
-	system("cls");
-	if(respuesta == 1){
-        for(i=0;tam_users>i;i++){
-        printf("Bienvenido al tutorial cientifico %s \n",usuarios[i].username);
-	}
-        printf("Durante todo este juego tendras problemas, \n");
-        printf("si los resuelves bien sobreviviras, si no resuelves los suficientes bien, moriras \n");
-        printf("Las preguntas se responderan con el numero 1 - 2 - 3 - 4 \n");
-        printf("Entonces, para que te acostumbres a la supervivencia... \n");
-        printf("El edificio en el que te acabas de despertar se está derrumbando... \n");
-        printf("1- En la primera salida, no hay ningun zombie pero tardas 30 minutos en salir \n");
-        printf("2- En la segunda salida, hay una horda de 6 zombies, pero tienes un AK-47 con 5 balas \n");
-        printf("3- En la tercera salida, hay un comboy del gobierno que te llevara a un lugar seguro \n");
-        printf("4- En la cuarta salida llegas a la terraza del edificio y tu unica opcion es lanzarte desde ahi \n");
-        do{
-        printf("Cual es la que tendra mayor indice de supervivencia? \n");
-        scanf("%d",&respuesta);
-        if(respuesta == 3){
-            printf("Bien hecho, has conseguido salir del hospital");
-        }else{
-            printf("Has muerto de una manera catastrofica (responde la 3)");
+        if(respuesta==1){
+                fflush(stdin);
+                pfile=fopen("ficheru.txt","a");
+                printf("Escriba su primer nombre cientifico %d \n",i+1);
+                gets(usuarios[i].nombre);
+                printf("Escriba su apellido \n");
+                gets(usuarios[i].apellido);
+                printf("Escriba su email \n");
+                gets(usuarios[i].email);
+                printf("Escriba su username \n");
+                gets(usuarios[i].username);
+                fprintf(pfile,"%s %s %s %s %s %d \n",usuarios[i].nombre,usuarios[i].apellido,usuarios[i].email,usuarios[i].username,&usuarios[i].puntuacion);
+                fclose(pfile);
         }
-        }while(respuesta!=3);
+        else if(respuesta==2){
+                fflush(stdin);
+                printf("Digame su nombre de usuario \n");
+                gets(usuarios[i].username);
+                j=ntotalg;
+            while(j>=0){
+                orden=strcmp(usuarios[i].username,usuariosg[j].username);
+                if(orden==0){
+                    printf("Bienvenido %s, te echabamos de menos, su ultima puntuacion es %d\n",usuariosg[j].username,usuariosg[j].puntuacion);
+                    usuarios[i].puntuacion=0;
+                    break;
+                }else{
+                    j--;
+                }
+                }
+            if(orden!=0){
+                    printf("no encontramos su nombre de usuario \n");
+                    i--;
+                }
 
-	}
+            if(orden==0){
+                pfile=fopen("ficheru.txt","a");
+                    if(pfile==NULL){
+                        printf("error");
+                        return 0;}
+                fprintf(pfile,"\n");
+                fprintf(pfile,"%s %s %s %s %d \n",usuariosg[j].nombre,usuariosg[j].apellido,usuariosg[j].email,usuarios[i].username,usuarios[i].puntuacion);
+                fclose(pfile);
+                }
+            }
+
+            i++;
+        }
 	//solo para cuando solo haya menu
 		printf("\nJugar de nuevo? SI(1) NO(3)\n");
 			scanf("%d",&menu);
